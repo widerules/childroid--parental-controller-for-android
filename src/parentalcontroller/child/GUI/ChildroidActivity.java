69f44307ac6a86170a677cf9d087de23d3ca0590
gckarunarathne <gckarunarathne@764be831-051c-de74-b99e-6b75132dbb8d>
2012-02-29 18:06:24 +0000

    send email to gmail added to the project. One piece of code works with the phone. Another piece works with K9 app.

--- src/parentalcontroller/child/GUI/ChildroidActivity.java
+++ src/parentalcontroller/child/GUI/ChildroidActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 public class ChildroidActivity extends Activity {
 	private Button btmain;
 	private Button btmsg;
+	private Button btemail;
+	private Button btmap;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -21,7 +23,7 @@ public class ChildroidActivity extends Activity {
 		btmain.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View arg0) {
 				Intent i = new Intent(ChildroidActivity.this,
-						SMSReceiveActivity.class);
+						MapViewActivity.class);
 				startActivity(i);
 			}
 		});
@@ -31,6 +33,50 @@ public class ChildroidActivity extends Activity {
 			public void onClick(View arg0) {
 				Intent l = new Intent(ChildroidActivity.this,
 						SMSSendActivity.class);
+				startActivity(l);
+			}
+		});
+		
+		this.btemail = (Button) findViewById(R.id.bSendMail);
+		this.btemail.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				
+				Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);  
+				  
+				String aEmailList[] = { "ak47gc@gmail.com","jayani2010@gmail.com" };  
+				String aEmailCCList[] = { "ak47gc@gmail.com","jayani2010@gmail.com"};  
+				String aEmailBCCList[] = { "ak47gc@gmail.com","jayani2010@gmail.com" };  
+				  
+				emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, aEmailList);  
+				emailIntent.putExtra(android.content.Intent.EXTRA_CC, aEmailCCList);  
+				emailIntent.putExtra(android.content.Intent.EXTRA_BCC, aEmailBCCList);  
+				  
+				emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "My subject");  
+				  
+				emailIntent.setType("plain/text");  
+				emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "My message body.");  
+				  
+				startActivity(emailIntent); 
+				
+				/*final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                
+                emailIntent.setType("plain/text");
+           
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{ "ak47gc@gmail.com","jayani2010@gmail.com"});
+         
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Test msg");
+         
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, "Hurray, its working....");
+ 
+                startActivity(Intent.createChooser(emailIntent, "Send mail..."));*/
+			}
+		});
+		
+		this.btmap = (Button) findViewById(R.id.buttonShowMap);
+		this.btmap.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				Intent l = new Intent(ChildroidActivity.this,
+						MapViewActivity.class);
 				startActivity(l);
 			}
 		});
