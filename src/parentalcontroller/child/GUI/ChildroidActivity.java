c36d8c78f80f4707475f74ddacbb4fbc2fb25d71
ak47gc <ak47gc@764be831-051c-de74-b99e-6b75132dbb8d>
2012-04-01 18:03:47 +0000

    Childroid completed. User screen prepared. Services are not going to show Activities now.

--- src/parentalcontroller/child/GUI/ChildroidActivity.java
+++ src/parentalcontroller/child/GUI/ChildroidActivity.java
@@ -34,87 +34,15 @@ public class ChildroidActivity extends Activity {
 		btmain = (Button) findViewById(R.id.buttonMain);
 		btmain.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View arg0) {
-				// Intent i = new Intent(ChildroidActivity.this,
-				// MapViewActivity.class);
-				// startActivity(i);
-				// Intent intent = new Intent(ChildroidActivity.this,
-				// DeviceAdmin.class);
-				// startService(intent);
+				
 				Toast.makeText(getBaseContext(),
 						"The custom preference has been clicked",
 						Toast.LENGTH_LONG).show();
 
 				authenticate();
-					Log.d("authentication", "yes, view ");
-				
-				
+					Log.d("authentication", "yes, view ");								
 			}
-		});
-		/*
-		 * send the msg to parentdroid
-		 */
-		this.btmsg = (Button) findViewById(R.id.showmsgbutton);
-		this.btmsg.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View arg0) {
-				Intent l = new Intent(ChildroidActivity.this,
-						SMSSendActivity.class);
-				startActivity(l);
-			}
-		});
-
-		/*
-		 * send email to the parentdroid
-		 */
-		this.btemail = (Button) findViewById(R.id.bSendMail);
-		this.btemail.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View arg0) {
-
-				Intent emailIntent = new Intent(
-						android.content.Intent.ACTION_SEND);
-
-				String aEmailList[] = { "ak47gc@gmail.com",
-						"jayani2010@gmail.com" };
-				String aEmailCCList[] = { "ak47gc@gmail.com",
-						"jayani2010@gmail.com" };
-				String aEmailBCCList[] = { "ak47gc@gmail.com",
-						"jayani2010@gmail.com" };
-
-				emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL,
-						aEmailList);
-				emailIntent.putExtra(android.content.Intent.EXTRA_CC,
-						aEmailCCList);
-				emailIntent.putExtra(android.content.Intent.EXTRA_BCC,
-						aEmailBCCList);
-
-				emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-						"My subject by Gihan");
-
-				emailIntent.setType("plain/text");
-				emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-						"My message body by Gihan.");
-
-				startActivity(emailIntent);
-
-				/*
-				 * final Intent emailIntent = new
-				 * Intent(android.content.Intent.ACTION_SEND);
-				 * 
-				 * emailIntent.setType("plain/text");
-				 * 
-				 * emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new
-				 * String[]{ "ak47gc@gmail.com","jayani2010@gmail.com"});
-				 * 
-				 * emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT,
-				 * "Test msg");
-				 * 
-				 * emailIntent.putExtra(android.content.Intent.EXTRA_TEXT,
-				 * "Hurray, its working....");
-				 * 
-				 * startActivity(Intent.createChooser(emailIntent,
-				 * "Send mail..."));
-				 */
-			}
-		});
+		});		
 
 		/*
 		 * show the map location of childroid
@@ -125,11 +53,7 @@ public class ChildroidActivity extends Activity {
 				Intent l = new Intent(ChildroidActivity.this,
 						MapViewActivity.class);
 				startActivity(l);
-
-				// startService(new Intent(getBaseContext(), MyService.class));
-
 				Log.d("gihan", "clicked map show");
-
 			}
 		});
 	}
--- src/parentalcontroller/child/Logic/SMSReceiveService.java
+++ src/parentalcontroller/child/Logic/SMSReceiveService.java
@@ -5,7 +5,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
 
 /*
  * This message listen for system broadcast messages
@@ -13,12 +16,15 @@ import android.telephony.SmsMessage;
 public class SMSReceiveService extends BroadcastReceiver {
 
 	private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
-
+	private String tag="SMSReceiveService";
+	
 	@Override
 	public void onReceive(Context context, Intent intent) {
-
+		
 		if (intent.getAction().equals(SMS_RECEIVED)) {//chaeck weather sms received
-
+			
+			Log.d(tag, "Received a msg");
+			
 			Bundle bundle = intent.getExtras();//get the additional information add with intent
 			if (bundle != null && bundle.containsKey("pdus")) {
 				Object[] pdusObj = (Object[]) bundle.get("pdus");//get the msg objects
@@ -43,10 +49,29 @@ public class SMSReceiveService extends BroadcastReceiver {
 							.getDisplayMessageBody(); // has the actual message
 					a[c] = displayOriginatingAddress + ":" + displayMessageBody;
 					c++;
-				}
-				i.putExtra("msgset", a);//adding msg data to send for the activity
-				context.startActivity(i);
+					Log.d(tag, "Message count :"+c +" "+a[c-1]);
+				}								
+				
+				Toast.makeText(context,
+						"Message count :"+c +" "+a[c-1],
+						Toast.LENGTH_LONG).show();
+				
+				//i.putExtra("msgset", a);//adding msg data to send for the activity
+				//context.startActivity(i);
 			}
+		}
+	}
+	
+	private void sendSMSParent(String smsNumberToSend, String smsTextToSend) {
+
+		SmsManager smsManager = SmsManager.getDefault();
+		try {
+			smsTextToSend += "Generated msg:\"" + smsTextToSend
+					+ "\"by childroid";
+			smsManager.sendTextMessage(smsNumberToSend, null, smsTextToSend,
+					null, null);
+		} catch (IllegalArgumentException ix) {
+
 		}
 	}
 }
